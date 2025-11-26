0af5d02d64182aec70241135371818035a68ee55
donfaruco <donfaruco@gmail.com>
2025-11-26 12:37:21 -0800

    Update outputs.tf
    fixing output.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,7 @@
 output "ec2_public_ip" {
-  value = aws_instance.color-app.public_ip
-  value = aws_instance.color-app-docker.public_ip
+  value = [aws_instance.color-app.public_ip, aws_instance.color-app-docker.public_ip]
 }
 
 output "ec2_public_dns" {
-  value = aws_instance.color-app.public_dns
-  value = aws_instance.color-app-docker.public_dns
+  value = [aws_instance.color-app.public_dns, aws_instance.color-app.public_dns]
 }
