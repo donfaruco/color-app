c7a0de9bf27ee664e540078a712a2ce0d6f7d497
donfaruco <donfaruco@gmail.com>
2025-11-26 12:10:24 -0800

    adding second ec2 instance for docker

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -8,5 +8,20 @@ resource "aws_instance" "color-app" {
 
   tags = {
     Name = var.instance_name
+    deployment = apache
+  }
+}
+
+resource "aws_instance" "color-app" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.ssh-key
+  vpc_security_group_ids = [aws_security_group.color-app.id]
+
+  user_data = file("scripts/userdata4docker.sh")
+
+  tags = {
+    Name = var.instance_name
+    deployment = docker
   }
 }
