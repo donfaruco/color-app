362e9583c400b09747c2ec344126b28577e85a3a
donfaruco <donfaruco@gmail.com>
2025-11-26 12:27:48 -0800

    adding second instance

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -7,21 +7,21 @@ resource "aws_instance" "color-app" {
   user_data = file("scripts/userdata.sh")
 
   tags = {
-    Name = var.instance_name
+    Name = color-app
     deployment = apache
   }
 }
 
-resource "aws_instance" "color-app" {
+resource "aws_instance" "color-app-docker" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   key_name               = var.ssh-key
-  vpc_security_group_ids = [aws_security_group.color-app.id]
+  vpc_security_group_ids = [aws_security_group.color-app-docker.id]
 
   user_data = file("scripts/userdata4docker.sh")
 
   tags = {
-    Name = var.instance_name
+    Name = color-app-docker
     deployment = docker
   }
 }
